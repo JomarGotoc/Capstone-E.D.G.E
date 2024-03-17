d1d5678fd12051c5eb7c3212f10df090b630113a
stephmislang <mislangstephanie@gmail.com>
2024-03-17 22:44:32 +0800

    fjfgjhmj

--- west_central_ii_elementary_school/manage_student/view_studentlist2.js
+++ west_central_ii_elementary_school/manage_student/view_studentlist2.js
@@ -1,15 +1,17 @@
-// Function to open the modal and display the overlay
-document.getElementById("addRecordButton").addEventListener("click", function() {
-    var overlay = document.getElementById("overlay");
-    var popupForm = document.getElementById("popupForm");
-    overlay.style.display = "block";
-    popupForm.style.display = "block";
-});
-
-// Function to close the modal and hide the overlay
-function closeModal() {
-    var overlay = document.getElementById("overlay");
-    var popupForm = document.getElementById("popupForm");
-    overlay.style.display = "none";
-    popupForm.style.display = "none";
+function togglePopupForm() {
+    var overlay = document.getElementById('overlay');
+    var popup = document.getElementById('popupForm');
+    overlay.style.display = 'block';
+    popup.style.display = 'block';
 }
+
+
+function closeModal() {
+    var overlay = document.getElementById('overlay');
+    var popup = document.getElementById('popupForm');
+    overlay.style.display = 'none';
+    popup.style.display = 'none';
+}
+
+
+document.getElementById('addRecordButton').addEventListener('click', togglePopupForm);
