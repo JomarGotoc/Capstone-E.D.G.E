72e83ae5625ebd6b949674d309b336d6874060b2
Jomar Parchamento Gotoc <jopa.gotoc.up@phinmaed.com>
2024-03-18 03:34:17 +0800

    Update template.php

--- west_central_ii_elementary_school/manage_student/template.php
+++ west_central_ii_elementary_school/manage_student/template.php
@@ -115,7 +115,7 @@ if ($result1->num_rows > 0) {
             justify-content: center;
             align-items: center;
             height: 100vh;
-            background: url(../../img/bg.png);
+            background: url(../../../img/bg.png);
             background-size: cover;
         }
         
@@ -573,7 +573,7 @@ if ($result1->num_rows > 0) {
     <header>
         <div class="container">
             <div class="header-content">
-                <img src="../../img/logo.png" class="logs">
+                <img src="../../../img/logo.png" class="logs">
                 <h4>E.D.G.E | P.A.R. Early Detection and Guidance for Education</h4>
                 <i class="vertical-line"></i>
                 <div class="dropdown">
