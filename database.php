7f4f1fb7586804ec1d63b35ed56db60ab56c4f51
IvanDanielCarino1 <ivza.carino.up@phinmaed.com>
2024-03-18 02:28:58 +0800

    ol db

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "edge";
+$servername = "sql6.freesqldatabase.com";
+$username = "sql6692134";
+$password = "8qZh8s4SRJ";
+$database = "sql6692134";
 $conn = new mysqli($servername, $username, $password, $database);
 ?>
