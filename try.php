5c55c3cf4c38e25cfc20a3b02307704f2cc27d1d
IvanDanielCarino1 <ivza.carino.up@phinmaed.com>
2024-04-22 11:02:20 +0800

    checkbox

---
+++ try.php
@@ -0,0 +1,6 @@
+<form>
+  <input type="checkbox" name="checkboxGroup" value="option1"> Option 1 <br>
+  <input type="checkbox" value="option2"> Option 2 <br>
+  <input type="checkbox" value="option3"> Option 3 <br>
+  <input type="checkbox" value="option4"> Option 4 <br>
+</form>
